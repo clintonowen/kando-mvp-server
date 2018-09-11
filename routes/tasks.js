8e69855c2afb135ba5670ef40836a3190b86451f
Clinton Owen <clint@clintonowen.com>
2018-09-11 14:02:10 -0500

    Link GET all tasks route to db

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,41 +1,27 @@
 'use strict';
 
 const express = require('express');
+const mongoose = require('mongoose');
+
+const Task = require('../models/task');
+const Column = require('../models/column');
 
 const router = express.Router();
 
 /* ========== GET/READ ALL TASKS ========== */
 router.get('/', (req, res, next) => {
-  res.json([
-    {
-      _id: '222222222222222222222201',
-      name: 'Example Task 1',
-      time: 0,
-      columnId: '111111111111111111111101',
-      userId: '000000000000000000000001'
-    },
-    {
-      _id: '222222222222222222222202',
-      name: 'Example Task 2',
-      time: 3900000,
-      columnId: '111111111111111111111101',
-      userId: '000000000000000000000001'
-    },
-    {
-      _id: '222222222222222222222203',
-      name: 'Example Task 3',
-      time: 1380000,
-      columnId: '111111111111111111111101',
-      userId: '000000000000000000000001'
-    },
-    {
-      _id: '222222222222222222222204',
-      name: 'Example Task 4',
-      time: 5380000,
-      columnId: '111111111111111111111102',
-      userId: '000000000000000000000001'
-    }
-  ]);
+  // const userId = req.user.id;
+
+  // let filter = { userId };
+
+  Task.find()
+    // .sort({ id: 'desc' })
+    .then(results => {
+      res.json(results);
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 module.exports = router;
