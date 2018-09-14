cb706afb0c3c625f37dea84c9ed849fe7a3f99de
Clinton Owen <clint@clintonowen.com>
2018-09-13 21:17:39 -0500

    Write all /api/columns endpoints

--- routes/columns.js
+++ routes/columns.js
@@ -4,8 +4,55 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const Column = require('../models/column');
+const User = require('../models/user');
 
 const router = express.Router();
+
+function validateColumnId(columnId, userId) {
+  if (columnId === undefined) {
+    const err = new Error('Missing `columnId`');
+    err.status = 400;
+    return Promise.reject(err);
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(columnId)) {
+    const err = new Error('The `columnId` is not valid');
+    err.status = 400;
+    return Promise.reject(err);
+  }
+
+  return Column.count({ _id: columnId, userId })
+    .then(count => {
+      if (count === 0) {
+        const err = new Error('The `columnId` is not valid');
+        err.status = 400;
+        return Promise.reject(err);
+      }
+    });
+}
+
+function validateUserId(userId) {
+  if (userId === undefined) {
+    const err = new Error('Missing `userId`');
+    err.status = 400;
+    return Promise.reject(err);
+  }
+
+  if (!mongoose.Types.ObjectId.isValid(userId)) {
+    const err = new Error('The `userId` is not valid');
+    err.status = 400;
+    return Promise.reject(err);
+  }
+
+  return User.count({ _id: userId })
+    .then(count => {
+      if (count === 0) {
+        const err = new Error('The `userId` is not valid');
+        err.status = 400;
+        return Promise.reject(err);
+      }
+    });
+}
 
 /* ========== GET/READ ALL COLUMNS ========== */
 router.get('/', (req, res, next) => {
@@ -13,7 +60,8 @@ router.get('/', (req, res, next) => {
 
   let filter = { userId };
 
-  Column.find(filter)
+  validateUserId(userId)
+    .then(() => Column.find(filter))
     // .sort({ id: 'desc' })
     .then(results => {
       res.json(results);
@@ -23,10 +71,59 @@ router.get('/', (req, res, next) => {
     });
 });
 
-/* ========== PUT/UPDATE TASK ========== */
+/* ========== GET/READ A SINGLE COLUMN ========== */
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const userId = req.user.id;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+
+  validateUserId(userId)
+    .then(() => Column.findOne({ _id: id, userId }))
+    .then(result => {
+      if (result) {
+        res.json(result);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+/* ========== POST/CREATE COLUMN ========== */
+router.post('/', (req, res, next) => {
+  const { name } = req.body;
+  const userId = req.user.id;
+
+  /***** Never trust users - validate input *****/
+  if (!name) {
+    const err = new Error('Missing `name` in request body');
+    err.status = 400;
+    return next(err);
+  }
+
+  const newColumn = { name, userId };
+
+  validateUserId(userId)
+    .then(() => Column.create(newColumn))
+    .then(result => {
+      res.location(`${req.originalUrl}/${result.id}`).status(201).json(result);
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+/* ========== PUT/UPDATE COLUMN ========== */
 router.put('/:id', (req, res, next) => {
   const { id } = req.params;
-  // const userId = req.user.id;
+  const userId = req.user.id;
 
   const toUpdate = {};
   const updateableFields = ['name'];
@@ -43,13 +140,39 @@ router.put('/:id', (req, res, next) => {
     return next(err);
   }
 
-  Column.findByIdAndUpdate(id, toUpdate, { new: true })
+  Promise.all([
+    validateUserId(userId),
+    validateColumnId(id, userId)
+  ])
+    .then(() => Column.findByIdAndUpdate(id , toUpdate, { new: true }))
     .then(result => {
       if (result) {
         res.json(result);
       } else {
         next();
       }
+    })
+    .catch(err => {
+      next(err);
+    });
+});
+
+/* ========== DELETE/REMOVE A SINGLE TASK ========== */
+router.delete('/:id', (req, res, next) => {
+  const { id } = req.params;
+  const userId = req.user.id;
+
+  /***** Never trust users - validate input *****/
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    const err = new Error('The `id` is not valid');
+    err.status = 400;
+    return next(err);
+  }
+
+  validateUserId(userId)
+    .then(() => Column.findOneAndRemove({ _id: id, userId }))
+    .then(() => {
+      res.sendStatus(204);
     })
     .catch(err => {
       next(err);
